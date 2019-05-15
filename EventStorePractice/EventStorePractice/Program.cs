c45f22b17563ade41b6a5beaa71d050e5f9584df
RupeshBandekar <rupesh24688@gmail.com>
2019-05-15 21:59:17 +0530

    removed event store exercise project

--- EventStorePractice/EventStorePractice/Program.cs
+++ EventStorePractice/EventStorePractice/Program.cs
@@ -79,6 +79,117 @@ namespace EventStorePractice
             //Console.WriteLine(Encoding.UTF8.GetString(readResult.Event.Value.Event.Data));
         }
 
+        static void ConnectToPersistentSubscriptions()
+        {
+            //var conn = CreateConnection();
+            //var streamName = Globals.streamName;
+            //var adminCredentials = Globals.AdminCredentials;
+
+            var conn = CreateConnection();
+            var adminCredentials = Globals.AdminCredentials;
+            var projection = Globals.Projection;
+            var streamName = string.Empty;
+
+            foreach (var _file in Directory.GetFiles("../", "shoppingCart-*"))
+            {
+                streamName = Path.GetFileNameWithoutExtension(_file);
+                var eventDataRaw = ProcessEvents(_file);
+                var eventData = eventDataRaw.ToArray();
+                conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Wait();
+            }
+
+            PersistentSubscriptionSettings settings = PersistentSubscriptionSettings.Create()
+                .DoNotResolveLinkTos()
+                .StartFromCurrent();
+
+            conn.CreatePersistentSubscriptionAsync(streamName, "examplegroup", settings, adminCredentials).Wait();
+
+            conn.ConnectToPersistentSubscription(streamName, "examplegroup", (_, x) =>
+            {
+                var data = Encoding.ASCII.GetString(x.Event.Data);
+                Console.WriteLine("Received: " + x.Event.EventStreamId + ":" + x.Event.EventNumber);
+                Console.WriteLine(data);
+            }, (sub, reason, ex) => { }, adminCredentials);
+
+            Console.WriteLine("waiting for events. press enter to exit");
+            Console.ReadLine();
+        }
+
+        static void FirstProjection()
+        {
+            var conn = CreateConnection();
+            var adminCredentials = Globals.AdminCredentials;
+            var projection = Globals.Projection;
+
+            foreach (var _file in Directory.GetFiles("../","shoppingCart-*"))
+            {
+                var streamName = Path.GetFileNameWithoutExtension(_file);
+                var eventDataRaw = ProcessEvents(_file);
+                var eventData = eventDataRaw.ToArray();
+                conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Wait();
+            }
+
+            string countItemsProjection = @"
+                    fromAll().when({
+                    $init: function(){
+                        return {
+                            count: 0
+                        }
+                    },
+                    ItemAdded: function(s,e){
+                        if(e.body.Description.indexOf('Xbox One S') >= 0){
+                            s.count += 1;
+                        }
+                    }
+                })";
+
+            projection.CreateContinuousAsync("xbox-one-s-counter", countItemsProjection, adminCredentials).Wait();
+
+            var projectionState = projection.GetStateAsync("xbox-one-s-counter", adminCredentials);
+            Console.Write(projectionState.Result);
+        }
+
+        static void CreateCatchupSubscription()
+        {
+            var conn = CreateConnection();
+            var adminCredentials = Globals.AdminCredentials;
+            var streamName = string.Empty;
+
+            foreach (var _file in Directory.GetFiles("../", "shoppingCart-*"))
+            {
+                streamName = Path.GetFileNameWithoutExtension(_file);
+
+                var sub = conn.SubscribeToStreamFrom(streamName, null, true,
+                    (_, x) =>
+                    {
+                        string data = Encoding.ASCII.GetString(x.Event.Data);
+                        Console.WriteLine("Received: " + x.Event.EventStreamId + ":" + x.Event.EventNumber);
+                        Console.WriteLine(data);
+                    });
+            }
+
+            Console.WriteLine("waiting for events. press enter to exit");
+            Console.ReadLine();
+
+        }
+
+        static void WriteData()
+        {
+            var conn = CreateConnection();
+            var adminCredentials = Globals.AdminCredentials;
+            var streamName = string.Empty;
+
+            foreach (var _file in Directory.GetFiles("../", "shoppingCart-*"))
+            {
+                streamName = Path.GetFileNameWithoutExtension(_file);
+                var eventDataRaw = ProcessEvents(_file);
+                var eventData = eventDataRaw.ToArray();
+                conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Wait();
+            }
+            Console.WriteLine("events written successfully! press enter to exit");
+            Console.ReadLine();
+        }
+
         public EventData AddSalesData(int productId, string productName, int quantity, decimal price)
         {
             return new EventData(Guid.NewGuid(), "salesdata", true,
@@ -88,8 +199,35 @@ namespace EventStorePractice
 
         static void Main(string[] args)
         {
-            FirstEvent();
-            ReadFirstEvent();
+            Console.WriteLine("1 - Connect to persistent subscription");
+            Console.WriteLine("2 - Writing first event");
+            Console.WriteLine("3 - First projection");
+            Console.WriteLine("4 - Create Catchup Subscription");
+            Console.WriteLine("5 - Write Data");
+            Console.WriteLine("Enter your option:");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                FirstEvent();
+                ReadFirstEvent();
+            }
+            else if(input == "2")
+            {
+                ConnectToPersistentSubscriptions();
+            }
+            else if(input == "3")
+            {
+                FirstProjection();
+            }
+            else if(input == "4")
+            {
+                CreateCatchupSubscription();
+            }
+            else if(input == "5")
+            {
+                WriteData();
+            }
+            
         }
 
     }
