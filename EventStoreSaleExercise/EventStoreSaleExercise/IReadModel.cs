db3ea5e8605d76bab99d4a875386a7baf5cf84d6
RupeshBandekar <rupesh24688@gmail.com>
2019-05-15 22:01:15 +0530

    moved even store exercies in new branch

---
+++ EventStoreSaleExercise/EventStoreSaleExercise/IReadModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EventStoreSaleExercise
+{
+    using System;
+    using EventStore.ClientAPI;
+    public interface IReadModel
+    {
+        void Subscribe();
+
+        void ReceivedEvent(EventStoreCatchUpSubscription subscription, ResolvedEvent evt);
+
+        void ReadAllSaleAddedEvents();
+
+        void Dropped(EventStoreCatchUpSubscription subscription, SubscriptionDropReason reason, Exception ex);
+    }
+}
---
+++ EventStoreSaleExercise/EventStoreSaleExercise/Sales.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EventStore.ClientAPI;
+using Newtonsoft.Json;
+
+namespace EventStoreSaleExercise
+{
+    public class Sales
+    {
+        public readonly Guid Id;
+        public readonly string ProductName;
+        public readonly int Quantity;
+        public readonly decimal Price;
+
+        public Sales(string productName, int quantity, decimal price)
+        {
+            Id = Guid.NewGuid();
+            ProductName = productName.ToUpper();
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public void AddSale(Sales objSales)
+        {
+            var conn = Globals.CreateConnection();
+            var streamName = "Sales";
+            var eventData = ProcessSalesData(objSales);
+            conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Wait();
+            Console.WriteLine($"Sale published on {streamName} stream.");
+        }
+
+        private List<EventData> ProcessSalesData(Sales objSales)
+        {
+            var events = JsonConvert.SerializeObject(objSales);
+            var eventData = new List<EventData>();
+            
+                var id = Guid.NewGuid().ToString();
+                var eventType = Globals.SaleAddedEvent;
+                eventData.Add(new EventData(Guid.Parse(id), eventType, true,
+                    Encoding.UTF8.GetBytes(events), null));
+           
+
+            return eventData;
+        }
+    }
+}
