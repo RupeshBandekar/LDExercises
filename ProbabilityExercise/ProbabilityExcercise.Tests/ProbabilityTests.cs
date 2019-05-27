f81859ee3e56bb52076ea0b53a879862317503ad
Rupesh Bandekar <rupesh24688@gmail.com>
2019-05-27 19:47:57 +0530

    created new branch for Probability

---
+++ ProbabilityExercise/ProbabilityExcercise.Tests/ProbabilityTests.cs
@@ -0,0 +1,92 @@
+﻿namespace ProbabilityExercise.Tests
+{
+    using System;
+    using Xunit;
+
+    public class ProbabilityTests
+    {
+        [Theory]
+        [InlineData(1.5)]
+        [InlineData(-0.5)]
+        public void RaiseRangeExceptionProbability(decimal decProbA)
+        {
+            Assert.Throws<ArgumentException>(() => new Probability(decProbA));
+        }
+
+        [Theory]
+        [InlineData(0.1,0.2, 0.02)]
+        public void AndProbability(decimal decProbA, decimal decProbB, decimal expectedResult)
+        {
+            Probability p1 = new Probability(decProbA);
+            Probability p2 = new Probability(decProbB);
+            Probability oactResult = p1.And(p2);
+            Probability oexpResult = new Probability(expectedResult);
+            Assert.True(oactResult.Equals(oexpResult));
+        }
+
+        [Theory]
+        [InlineData(0.1, 0.2, 0.28)]
+        public void OrProbability(decimal decProbA, decimal decProbB, decimal expectedResult)
+        {
+            Probability p1 = new Probability(decProbA);
+            Probability p2 = new Probability(decProbB);
+            Probability oactResult = p1.Or(p2);
+            Probability oexpResult = new Probability(expectedResult);
+            Assert.True(oactResult.Equals(oexpResult));
+        }
+        
+        [Theory]
+        [InlineData(0.5, 0.5)]
+        public void InverseProbability(decimal decProbA, decimal expectedResult)
+        {
+            Probability p1 = new Probability(decProbA);
+            Assert.Equal(expectedResult, p1.Inverse());
+        }
+
+        [Theory]
+        [InlineData(0.1, 0.1)]
+        public void CheckEqualsOperator(decimal decProbA, decimal decProbB)
+        {
+            Probability p1 = new Probability(decProbA);
+            Probability p2 = new Probability(decProbB);
+            Assert.True(p1 == p2);
+        }
+
+        [Theory]
+        [InlineData(0.1, 0.2)]
+        public void CheckNotEqualsOperator(decimal decProbA, decimal decProbB)
+        {
+            Probability p1 = new Probability(decProbA);
+            Probability p2 = new Probability(decProbB);
+            Assert.True(p1 != p2);
+        }
+
+         [Theory]
+         [InlineData(0.1,0.1,true)]
+         [InlineData(0.1, null, false)]
+        public void CheckNullObjectEquals(decimal decProbA, decimal decProbB, bool expectedResult)
+        {
+            Object p1 = new Probability(decProbA);
+            Object p2 = new Probability(decProbB);
+            Assert.Equal(expectedResult, p1.Equals(p2));
+        }
+
+        [Theory]
+        [InlineData(0.1,false)]
+        public void CheckProbabilityObjectEquals(decimal decProbA, bool expectedResult)
+        {
+            Object p1 = new Probability(decProbA);
+            Object p2 = null;
+            
+            Assert.Equal(expectedResult, p1.Equals(p2));
+        }
+
+        [Theory]
+        [InlineData(0.1)]
+        public void CheckHashCode(decimal decProbA)
+        {
+            Object p1 = new Probability(decProbA);
+            Assert.Equal(decProbA.GetHashCode(), p1.GetHashCode());
+        }
+    }
+}
---
+++ ProbabilityExercise/ProbabilityExcercise/Probability.cs
@@ -0,0 +1,57 @@
+﻿namespace ProbabilityExercise
+{
+    using System;
+    public struct Probability : IEquatable<Probability>
+    {
+        private readonly decimal _decNum;
+
+        public Probability(decimal decNum)
+        {
+            if (!(decNum >= 0 && decNum <= 1))
+                throw new ArgumentException("Probability should be in between 0 and 1");
+            _decNum = decNum;
+        }
+
+        public Probability And(Probability other)
+        {
+            return new Probability(_decNum * other._decNum);
+        }
+
+        public Probability Or(Probability other)
+        {
+            return new Probability(_decNum + other._decNum - (_decNum * other._decNum));
+        }
+
+        public decimal Inverse()
+        {
+            return 1 - _decNum;
+        }
+
+        public bool Equals(Probability other)
+        {
+            return _decNum == other._decNum;
+        }
+
+        public static bool operator ==(Probability left, Probability right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Probability left, Probability right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Probability other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _decNum.GetHashCode();
+        }
+    }
+
+}
