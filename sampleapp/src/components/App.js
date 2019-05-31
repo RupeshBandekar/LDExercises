4471501afee1885abf43c74702e5c616810bf21a
Rupesh Bandekar <rupesh24688@gmail.com>
2019-05-31 15:27:46 +0530

    structured components files

---
+++ sampleapp/src/components/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import Game from "./Game";
+import '../styles/App.css';
+
+const StarMatch = () =>{
+  const [gameId, setGameId] = useState(1);
+  return(
+    <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>
+  )
+};
+
+function App() {
+  return (
+    <StarMatch />
+  );
+}
+
+export default App;
