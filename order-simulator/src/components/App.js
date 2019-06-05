f16623dbbacc28de4be75d3554997421f463cfef
Rupesh Bandekar <rupesh24688@gmail.com>
2019-06-05 19:22:42 +0530

    added numeric validations and validation summary

--- order-simulator/src/components/App.js
+++ order-simulator/src/components/App.js
@@ -15,6 +15,9 @@ class PortfolioData extends React.Component{
         <div className="portfolio-column">
           {portfolioData.price}
         </div>
+        <div className="portfolio-column">
+          {portfolioData.quantity * portfolioData.price}
+        </div>
       </div>
     );
   }
@@ -36,6 +39,9 @@ class Portfolio extends React.Component{
       <div className="portfolio-column">
         Buy Price
       </div>
+      <div className="portfolio-column">
+        Current Price
+      </div>
       {this.props.portfolio.map(portfolioData => <PortfolioData key={portfolioData.id} {...portfolioData}/>)}
     </div>
     
@@ -45,19 +51,34 @@ class Portfolio extends React.Component{
 
 
 class SendOrder extends React.Component{
-  state = {txnDate: "", txnType: "0", asset: "0", quantity: "", price: "", txnStatus: "", failureReason: ""}
-  //{asset: '0', quantity: '', price: ''};
+  state = {txnDate: "", txnType: "0", asset: "0", quantity: "", price: "", txnStatus: "", failureReason: "", errorMessage: ""};
   handleSendOrder = event => {
     event.preventDefault();
     this.props.onSubmit(this.state);
-    //this.setState({action:'0', quantity: '', price: '', asset: '0'});
+    this.setState({txnDate: "", txnType: "0", asset: "0", quantity: "", price: "", txnStatus: "", failureReason: ""});
   };
 
-  validateQuantity = (event) => {    
-    const re = /^[0-9\b]+$/;
-      if (event.target.value === '' || re.test(event.target.value)) {
-        this.setState({quantity: event.target.value});
-      }
+  acceptValidInput = (event) => {
+    switch(event.target.id)
+    {
+      case "txtQuantity": 
+        {
+          const re = /^[1-9]+\d*$/;
+          if (event.target.value === '' || re.test(event.target.value)) {
+            this.setState({quantity: event.target.value});
+          }
+        }
+        break;
+      case "txtPrice":
+        {
+          const re = /^[1-9]+\d*\.?\d*?$/;
+          if (event.target.value === '' || re.test(event.target.value)) {
+            this.setState({price: event.target.value});
+          }   
+        }
+        break;
+      default:
+    }
   }
 
   render(){
@@ -68,19 +89,20 @@ class SendOrder extends React.Component{
         </div>
         <div>
           <div className="sendorder-column">
-            <select id="ddlAction" value={this.state.txnType} onChange={event => this.setState({txnType: event.target.value})}>
+            <select id="ddlAction" value={this.state.txnType} 
+            onChange={event => {this.setState({txnType: event.target.value}); this.props.onChange();}} required>
               <option value="">Action</option>
               <option value="B">Buy</option>
               <option value="S">Sell</option>
             </select>          
           </div>
           <div className="sendorder-column">
-          <input id="txtQuantity" type="text" value={this.state.quantity}  placeholder="Quantity" 
-          onChange={event => this.validateQuantity(event)} required onInvalid="this.setCustomValidity('enter quantity')" onInput="this.setCustomValidity('')"></input>
+          <input id="txtQuantity" type="text" value={this.state.quantity} placeholder="Quantity" 
+          onChange={event => {this.acceptValidInput(event); this.props.onChange();}} required></input>
           </div>
           <div className="sendorder-column">
             Asset:
-            <select id="ddlAsset" value={this.state.asset} onChange={event => this.setState({asset: event.target.value})}>
+            <select id="ddlAsset" value={this.state.asset} onChange={event => {this.setState({asset: event.target.value}); this.props.onChange();}} required>
               <option value="">Select</option>
               <option value="GOOGL" title="	Alphabet Inc Class A">GOOGL</option>
               <option value="MO" title="Altria Group Inc">MO</option>
@@ -95,13 +117,14 @@ class SendOrder extends React.Component{
             </select>          
           </div>
           <div className="sendorder-column">
-            <input id="txtPrice" type="text" placeholder="Price" value={this.state.price} onChange={event => this.setState({price: event.target.value})}></input>
+            <input id="txtPrice" type="text" placeholder="Price" value={this.state.price} 
+            onChange={event => {this.acceptValidInput(event); this.props.onChange();}} required></input>
           </div>
         </div>
         <div>
-          <button>Send Order</button>
-          <label id="lblError"></label>
+          <button>Send Order</button>          
         </div>
+        <div>{this.state.errorMessage}</div>
       </form>
     );
   };
@@ -172,13 +195,21 @@ class TransactionHistory extends React.Component{
   };
 }
 
+class ValidationSummary extends React.Component {  
+  render(){
+    return(
+      <div>{this.props.errorMessage}</div>
+    );
+  };
+}
+
 class App extends React.Component {
 
   state = {
     portfolio: [
-      {quantity: 100, asset: "GOOGL", price: 250.00},
-      {quantity: 250, asset: "MO", price: 412.00},
-      {quantity: 325, asset: "AMZN", price: 645.25},
+      {asset: "GOOGL", quantity: 100, price: 200.00},
+      {asset: "MO", quantity: 200, price: 150.00},
+      {asset: "AMZN", quantity: 250, price: 250},
     ],
 
     txnHistory: [
@@ -186,19 +217,48 @@ class App extends React.Component {
       {txnDate: "6/3/2019 2:20:55 PM", txnType: "B", asset: "GOOGL", quantity: 500, price: 105.25, txnStatus: "S", failureReason: ""},
       {txnDate: "6/3/2019 2:22:30 PM", txnType: "S", asset: "GOOGL", quantity: 1005, price: 105.25, txnStatus: "F", failureReason: "Quantity for sell exceeds the quantity available"},
     ],
+
+    errorMessage: '',
   };
 
-  updatePortfolio = (portfolioData) => {
-    const item = this.state.portfolio.filter(x => x.asset === portfolioData.asset);
-    if(item.length > 0)
-    {
-      console.log(item);
+  updatePortfolio = (orderData) => {
+    
+    const index = this.state.portfolio.findIndex(x => x.asset === orderData.asset);
+    if(index > -1)
+    {      
+      const copyPortfolio = [...this.state.portfolio];
+      let copyPortfolioAsset = copyPortfolio[index];
+      let portfolioQuantity = parseInt(copyPortfolioAsset.quantity);
+      let orderQuantity = parseInt(orderData.quantity);
+      if(orderData.txnType === "S")
+      {
+        if(portfolioQuantity - orderQuantity === 0)
+        {
+          //removing asset from portfolio
+          const splicedPortfolio = this.state.portfolio.splice(index, 1);
+          this.setState({splicedPortfolio});
+        }
+        else {
+          portfolioQuantity = portfolioQuantity - orderQuantity;
+        }
+      }
+      else if(orderData.txnType === "B")
+      {
+        portfolioQuantity = portfolioQuantity + orderQuantity;
+      }
+      
+      if(parseInt(copyPortfolioAsset.quantity) !== portfolioQuantity)
+      {        
+        copyPortfolioAsset.quantity = portfolioQuantity;
+        copyPortfolio[index] = copyPortfolioAsset;
+        this.setState({copyPortfolio});
+      }
     }
     else {
       this.setState(prevPortfolio => ({
-        portfolio: [...prevPortfolio.portfolio, portfolioData],
+        portfolio: [...prevPortfolio.portfolio, orderData],
       }));
-    }    // console.log("inside App");
+    }
   };
 
   addNewTxnHistory = (txnData) => {
@@ -214,52 +274,77 @@ class App extends React.Component {
     }));
   };
 
-  
+  validateQuantity = (orderQuantity) => {    
+    const re = /^[1-9]+\d*$/;
+    if (!(orderQuantity === '' || re.test(orderQuantity))) {
+      return false;
+    }
+    else return true;
+  }
+
+  validatePrice = (orderPrice) => {
+    const re = /^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$/;
+    if (re.test(orderPrice)) {
+      return true;
+    } 
+    else return false;       
+  }
+
+  resetErrorMessage = () => {
+    this.setState({errorMessage: ''});
+  }
+
   validateOrder = (orderData) => {
-    //https://stackoverflow.com/questions/29537299/react-how-do-i-update-state-item1-on-setstate-with-jsfiddle
-    console.log("inside validate order");
-    console.log(orderData.txnType);
-    const portfolioPosition = this.state.portfolio.filter(x => x.asset === orderData.asset);
-    if(orderData.txnType === "S")
-    {      
+    //https://stackoverflow.com/questions/29537299/react-how-do-i-update-state-item1-on-setstate-with-jsfiddle    
+    
+    if(!(this.validatePrice(orderData.price))) {
+      this.setState({errorMessage: 'Invalid price'});
+    }
+    else {  
+      const portfolioPosition = this.state.portfolio.filter(x => x.asset === orderData.asset);
+      orderData.txnStatus = "S";
+
       if(portfolioPosition.length > 0)
       {
-        //if asset is present in portfolio
-        if(portfolioPosition[0].quantity < orderData.quantity)
+        orderData.price = portfolioPosition[0].price;
+      }
+
+      if(orderData.txnType === "S")
+      {      
+        if(portfolioPosition.length > 0)
         {
+          //if asset is present in portfolio
+          if(portfolioPosition[0].quantity < orderData.quantity)
+          {
+            orderData.txnStatus = "F";
+            orderData.failureReason = "Quantity for sell exceeds the portfolio quantity";
+            this.setState({errorMessage: 'Quantity for sell exceeds the portfolio quantity'});
+          }
+        }
+        else
+        {
+          //asset is not present in portfolio
           orderData.txnStatus = "F";
-          orderData.failureReason = "Quantity for sell exceeds the portfolio quantity";
+          orderData.failureReason = "Asset for sell does not exist in portfolio";
+          this.setState({errorMessage: 'Asset for sell does not exist in portfolio'});
         }
       }
-      else
-      {
-        //asset is not present in portfolio
-        orderData.txnStatus = "F";
-        orderData.failureReason = "Asset for sell does not exist in portfolio";
+      
+      this.addNewTxnHistory(orderData);
+
+      if(orderData.txnStatus === "S") {
+        console.log(portfolioPosition);
+        this.updatePortfolio(orderData);
       }
     }
-    
-    this.addNewTxnHistory(orderData);
-
-    if(orderData.txnStatus === "S") {
-      this.updatePortfolio(orderData);
-    }
-    
-
-    let portfolioItems = [...this.state.portfolio];
-    let portfolioItem = portfolioItems[0];
-    portfolioItem.quantity = 200;
-    portfolioItems[0] = portfolioItem;
-    this.setState({portfolioItems});
-    console.log(this.state.portfolio);
-    console.log(this.state.txnHistory);
   };
 
   render(){
     return (
       <div>        
         <Portfolio portfolio={this.state.portfolio}/>
-        <SendOrder onSubmit={this.validateOrder} />
+        <SendOrder onSubmit={this.validateOrder} onChange={this.resetErrorMessage}/>
+        <ValidationSummary errorMessage={this.state.errorMessage}/>
         <TransactionHistory txnHistory={this.state.txnHistory}/>
       </div>
     );
